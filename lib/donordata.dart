afe51c5f53906f15b1e6835e26778301b8251041
Wazid Hossain <mwh.wajid96@gmail.com>
2025-05-07 11:44:56 +0600

    #

--- lib/donordata.dart
+++ lib/donordata.dart
@@ -384,7 +384,7 @@ class DonorData {
       'contact': '01959-073034',
       'emergency': 'N/A',
       'address': 'Ishwardi, Pabna',
-      'lastDonation': '21th Epril 2025',
+      'lastDonation': '22th April 2025',
       'status': 'Unavailable',
       'image': 'assets/images/emon.jpg'
     },

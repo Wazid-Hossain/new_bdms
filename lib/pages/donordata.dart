d0d65748f154a0336c34f589a0a2b16d179de0c5
Wazid Hossain <mwh.wajid96@gmail.com>
2025-07-15 12:09:36 +0600

    #

--- lib/pages/donordata.dart
+++ lib/pages/donordata.dart
@@ -180,7 +180,7 @@ class DonorData {
       'image': 'assets/images/mithun.jpg'
     },
     {
-      'name': 'Kawser KKR',
+      'name': 'Kawser ngn',
       'bloodType': 'A+',
       'contact': '01798-183332',
       'emergency': 'N/A',
